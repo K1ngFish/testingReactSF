797c94648ff1fb44340c70af67212926a5947e7f
Denis Novikov <novikov.denis.v@gmail.com>
2023-12-31 19:30:37 +0300

    project initiated

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "../styles/App.css";
+
+function App () {
+    return (
+        <div>
+            <h1>Hello from Denis!</h1>
+        </div>
+    )
+}
+
+export default App;
