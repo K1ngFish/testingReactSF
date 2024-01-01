8105a4f72dccf588b137b34397566d68d0dd7af4
Denis Novikov <novikov.denis.v@gmail.com>
2024-01-01 18:54:55 +0300

    weather component added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+
 import "../styles/Main.css";
+import Weather from "./Weather.jsx";
 
 function Main() {
     return (
         <main>
             <h2>Main section</h2>
+            <Weather/>
         </main>
 
     )
---
+++ src/components/Weather.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+import "../styles/Weather.css"
+
+function Weather () {
+    return (
+        <h3>Select city</h3>
+    )
+}
+
+export default Weather;
